667e5f8d6c89e3e12cb2a58f359771241c999dca
AzBlue <214144484+AzBlue303@users.noreply.github.com>
2025-05-08 13:18:28 -0400

    hotfix: cambios necesarios para funcionamiento del plugin manager

--- src/plugins/CalcLTFC.ts
+++ src/plugins/CalcLTFC.ts
@@ -14,8 +14,6 @@ export class CalcLTFC implements PluginOptions {
 
     dataGraph: { dias: number[]; commit: string[] } = { dias: [], commit: [] };
     outputPath: string;
-    owner: string = "";
-    repo: string = "";
 
     constructor(srcRepoPath: string, srcOutpoutPath: string, yearRepo: string) {
         this.repoPath = srcRepoPath;
@@ -28,7 +26,7 @@ export class CalcLTFC implements PluginOptions {
         console.log("Calculando Lead Time for Changes...");
     }
 
-    async calcMetrics(...args: any[]): Promise<any> {
+    async calcMetrics(): Promise<any> {
         try {
 
             const git = simpleGit(this.repoPath);
@@ -98,4 +96,8 @@ export class CalcLTFC implements PluginOptions {
     terminate(): void {
         console.log("Calculo terminado, resultado: ", this.resultLTFC);
     }
+}
+
+export function createPlugin(params: {srcRepoPath: string, srcOutputPath: string, yearRepo: string}): PluginOptions {
+    return new CalcLTFC(params.srcRepoPath, params.srcOutputPath, params.yearRepo);
 }
